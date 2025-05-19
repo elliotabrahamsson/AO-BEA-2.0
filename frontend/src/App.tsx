0f83dc1f745c089e274a69bba1079e6452f10725
BickeB <briancastillo1998@hotmail.com>
2025-05-19 13:10:39 +0200

    Navbar, Dropdown menus started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import DropdownProducts from './components/DropdownProducts';
+import DropdownCare from './components/DropdownCare';
 
 function App() {
     return (
         <>
             <Home />
+            <DropdownProducts/>
+            <DropdownCare/>
+            <Navbar/>
         </>
     );
 }
