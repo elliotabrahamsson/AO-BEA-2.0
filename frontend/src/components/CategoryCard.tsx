7b3707e2bc7c29d56e661a747dbea7b9be59f6a0
AlexanderA02 <106035161+AlexanderA02@users.noreply.github.com>
2025-05-21 13:01:55 +0200

    Några modifieringar i backend index.ts, CategoryCard och CategoryRoute fixat. Det ända som saknas i CategoryRoute är New Arrivals.

--- frontend/src/components/CategoryCard.tsx
+++ frontend/src/components/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 type Props = {
   category: string;
@@ -12,25 +12,28 @@ export default function CategoryCard({
   shopType,
 }: Props) {
   return (
-    <div>
-      {/* Stand-ins för riktiga koden */}
-      {category}, {clothingImage}, {shopType}{" "}
-    </div>
+    <Link to={"/shop/" + shopType + "/" + category}>
+      <section
+        id="card"
+        className={`flex justify-center items-center w-[190px] min-h-[240px] m-[1em] bg-cover bg-center`}
+        // Eftersom att tailwind sätter all css under build time (inte run time) kan vi inte använda ex. bg-[url(${clothingImage})] eftersom att Tailwind inte kan läsa JavaScript variabler. Därför satte jag bakgrunden i style-attributet.
+        style={{ backgroundImage: `url(${clothingImage})` }}
+      >
+        <h3
+          className={
+            "bg-[var(--dark3)] text-[var(--bright1)] p-[0.4em] text-[80%]"
+          }
+          style={{ fontSize: "1.2em" }}
+        >
+          {category}
+        </h3>
+      </section>
+    </Link>
   );
 }
 
 {
-  /* <script setup>
-import { defineProps } from "vue";
-import { RouterLink } from "vue-router";
-
-const props = defineProps({
-  category: String,
-  clothingImg: Image,
-  shopType: String,
-});
-</script>
-
+  /*
 <template>
 
   <RouterLink :to="'/shop/' + shopType + '/' + category">
--- frontend/src/routes/CategoryRoute.tsx
+++ frontend/src/routes/CategoryRoute.tsx
@@ -1,9 +1,66 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CategoryCard from "../components/CategoryCard";
 
 export default function CategoryRoute() {
+  interface Product {
+    id: number;
+    category_type: string;
+    product_name: string;
+    brand_name: string;
+    product_description: string;
+    product_img: string;
+    price: number;
+    stock: number;
+    gender: string;
+    color: string[];
+    size: string[];
+  }
+
   const { store_type } = useParams();
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data: Product[]) => {
+        // Här kommer alla kategorier hamna. De kommer vara unika och komma från shoptypen som skickas med via params, dvs. herrmode eller dammode.
+        let uniqueCategoryData: Product[] = [];
+        // Filtrerar databasens data utifrån vilken shoptyp det är, dvs. herrmode eller dammode. Denna array innehåller alla produkter från en viss shoptyp, t.ex alla herrmode produkter.
+        const filteredData = data.filter((product) => {
+          if (store_type === "herrmode") {
+            return product.gender === "Man";
+          } else {
+            return product.gender === "Woman";
+          }
+        });
+        // Denna forEach kollar om en produkt-kategori finns i uniqueCategoryData, om den inte gör det kommer den läggas till i arrayen via en push. Detta gör att uniqueCategoryData kommer vara en array av objekt som alla har en unik kategori.
+        filteredData.forEach((product) => {
+          if (
+            !uniqueCategoryData.find(
+              (existingProduct) =>
+                existingProduct.category_type === product.category_type
+            )
+          ) {
+            uniqueCategoryData.push(product);
+            return;
+          } else {
+            if (
+              !uniqueCategoryData.find(
+                (existingProduct) =>
+                  existingProduct.category_type === product.category_type
+              )
+            ) {
+              uniqueCategoryData.push(product);
+              return;
+            }
+          }
+          return uniqueCategoryData;
+        });
+        setProducts(uniqueCategoryData);
+      });
+  }, []);
+
   return (
     <>
       <section className="bg-amber-100 p-[1em]">
@@ -14,30 +71,16 @@ export default function CategoryRoute() {
         )}
 
         <section id="categories_section">
-          <h2 className="text-center">KATEGORIER</h2>
+          <h2 className="text-center my-[1.5em]">KATEGORIER</h2>
 
           <div className="grid grid-cols-2 justify-items-center">
-            {/* Dessa ska bytas ut mot CategoryCard:s som ska genereras utifrån datan från databasen */}
-            <CategoryCard
-              category="Kategori"
-              clothingImage="Bild"
-              shopType="Shoptyp"
-            />
-            <CategoryCard
-              category="Kategori"
-              clothingImage="Bild"
-              shopType="Shoptyp"
-            />
-            <CategoryCard
-              category="Kategori"
-              clothingImage="Bild"
-              shopType="Shoptyp"
-            />
-            <CategoryCard
-              category="Kategori"
-              clothingImage="Bild"
-              shopType="Shoptyp"
-            />
+            {products.map((product) => (
+              <CategoryCard
+                category={product.category_type}
+                clothingImage={product.product_img}
+                shopType={product.gender}
+              />
+            ))}
           </div>
         </section>
       </section>
