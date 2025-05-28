ef443073af89a41d0f05175852b1f0082ffdd9ef
AntoniaNoaksson <antonianoaksson@gmail.com>
2025-05-28 14:07:41 +0200

    did useContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,68 @@
 /* import DropdownProducts from "./components/DropdownProducts";
 import DropdownCare from "./components/DropdownCare"; */
-import ShoppingCartRoute from "./routes/ShoppingCartRoute";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import HomeRoute from "./routes/HomeRoute";
-import Searchbar from "./components/Searchbar";
-import AboutRoute from "./routes/AboutRoute";
-import CategoryRoute from "./routes/CategoryRoute";
-import ProductPageRoute from "./routes/ProductPageRoute";
-import SelectedCategoryRoute from "./routes/SelectedCategoryRoute";
+import ShoppingCartRoute from './routes/ShoppingCartRoute';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import HomeRoute from './routes/HomeRoute';
+import Searchbar from './components/Searchbar';
+import AboutRoute from './routes/AboutRoute';
+import CategoryRoute from './routes/CategoryRoute';
+import ProductPageRoute from './routes/ProductPageRoute';
+import SelectedCategoryRoute from './routes/SelectedCategoryRoute';
 import Breadcrumb from './components/Breadcrumb';
 import CheckoutRoute from './routes/CheckoutRoute';
+import OrderConfirmationRoute from './routes/OrderConfirmationRoute';
+import LogInRoute from './routes/LogInRoute';
 import {
     createHashRouter,
     Link,
     Outlet,
     RouterProvider
 } from 'react-router-dom';
-import ScrollToTop from "./components/ScrollToTop";
-import DropdownProducts from "./components/DropdownProducts";
+import ScrollToTop from './components/ScrollToTop';
+import DropdownProducts from './components/DropdownProducts';
 
 function App() {
-  const router = createHashRouter([
-    {
-      // Detta (children) är en array av route-objekt, dvs. våra routes (views i Vue).
-      children: [
-        // Exempel på en route, element är det som ska visas, path är url:en.
-        { element: <HomeRoute />, path: "/" },
-        { element: <AboutRoute />, path: "/about" },
-        { element: <CategoryRoute />, path: "/shop/:store_type" },
-        { element: <ShoppingCartRoute />, path: "/shoppingcart" },
-        { element: <CheckoutRoute/> , path: '/checkout' },
+    const router = createHashRouter([
         {
-          element: <SelectedCategoryRoute />,
-          path: "/shop/:store_type/:selected_category",
-        },
-        {
-          element: <ProductPageRoute />,
-          path: "/shop/:store_type/:selected_category/:id",
-        },
+            // Detta (children) är en array av route-objekt, dvs. våra routes (views i Vue).
+            children: [
+                // Exempel på en route, element är det som ska visas, path är url:en.
+                { element: <HomeRoute />, path: '/' },
+                { element: <AboutRoute />, path: '/about' },
+                { element: <CategoryRoute />, path: '/shop/:store_type' },
+                { element: <ShoppingCartRoute />, path: '/shoppingcart' },
+                { element: <CheckoutRoute />, path: '/checkout' },
+                { element: <LogInRoute />, path: '/login' },
+                {
+                    element: <OrderConfirmationRoute />,
+                    path: '/orderconfirmation'
+                },
+                {
+                    element: <SelectedCategoryRoute />,
+                    path: '/shop/:store_type/:selected_category'
+                },
+                {
+                    element: <ProductPageRoute />,
+                    path: '/shop/:store_type/:selected_category/:id'
+                }
 
-
-        // Detta är enbart en exempel route.
-      ],
-      // Det som finns i element under denna mening är vad som alltid ska synas på sidan oavsett route, t.ex searchbar, footer, navbar.
-      element: (
-        <>
-          <ScrollToTop />
-          <Navbar />
-          {/* Här skulle man t.ex kunna lägga Searchbar komponenten */}
-          <header>
-            <Searchbar />
-            <Breadcrumb />
-          </header>
-          <main>
-            {/* Outlet motsvarar RouterView i Vue. Det är alltså här vi kommer visa upp våra olika route-komponenter beroende på url:en. T.ex kommer <HomeRoute /> komponenten synas här. */}
-            <Outlet />
-          </main>
+                // Detta är enbart en exempel route.
+            ],
+            // Det som finns i element under denna mening är vad som alltid ska synas på sidan oavsett route, t.ex searchbar, footer, navbar.
+            element: (
+                <>
+                    <ScrollToTop />
+                    <Navbar />
+                    {/* Här skulle man t.ex kunna lägga Searchbar komponenten */}
+                    <header>
+                        <Searchbar />
+                        <Breadcrumb />
+                    </header>
+                    <main>
+                        {/* Outlet motsvarar RouterView i Vue. Det är alltså här vi kommer visa upp våra olika route-komponenter beroende på url:en. T.ex kommer <HomeRoute /> komponenten synas här. */}
+                        <Outlet />
+                    </main>
 
                     {/* Link fungerar som RouterLink i Vue, det är i princip en a-tag */}
                     {/* <Link to="/exempel">Exempel</Link> */}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        {/*AuthProvider gör det möjligt för webbsidan att anpassas beroende på om man är användare eller ej  */}
+         <AuthProvider>
+            <App />
+        </AuthProvider>
+    </StrictMode>
 );
